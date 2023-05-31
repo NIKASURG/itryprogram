8ce4b6c8a19a7d11b6cb53bb42fdac2d8f9b102b
nikas <nsurgautas1@gmail.com>
2023-05-31 17:15:33 +0300

    komentaras

---
+++ pradzia.js
@@ -0,0 +1,10 @@
+Vue.component("s2-pradzia", {
+    // props: ["a", "b"],
+    props: {
+        // parametrai kuriuos paduosime komponentui
+    },
+    methods: {
+        // metodai kuriuos naudos šitas komponentas
+    },
+    template: `<div><p>Komponentas s2-pradžia užsikrovė</p></div>`,
+});
